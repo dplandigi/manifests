0c6073f95af9b1f0118b522e3ec27c0a6613a704
root <deepakl@quinnox.com>
2023-06-08 11:25:36 +0530

    Testing

--- site.pp
+++ site.pp
@@ -1,32 +1,13 @@
-node 'puppetclient'{
-
-  package { 'python':
-    ensure => latest,
-  }
-
-  package { 'ffmpeg':
-    ensure => latest,
-  }
-   
-
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get update',
-     path => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-   # path    => ['/usr/bin', '/usr/sbin'],
-    user    => 'root',
-    require => Package['python', 'ffmpeg'],
-  }
-}
-
 node 'desktop-1772sp6'{
 file { 'c:/dpl.txt':
     ensure   => present,
-    content  => 'The DPL Knowledgebase'
+    content  => 'Windows Puppet Agent Setup - Qyrus Infra'
    }
 }
+
 node 'testusers-ipro'{
-file { '/file1':
+file { '/Users/testuser/file1':
     ensure => present,
-    content => "Hello World",
+    content => "Mac Puppet Agent Setup - Qyrus Infra",
 }
 }
